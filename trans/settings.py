1783a56a2e2c01ccac01332754db72255a629186
Robin <42406671@qq.com>
2015-12-04 01:05:59 +0800

    增加STATIC_ROOT

--- trans/settings.py
+++ trans/settings.py
@@ -102,5 +102,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DOC_ROOT = os.path.join(BASE_DIR, 'django-docs')
